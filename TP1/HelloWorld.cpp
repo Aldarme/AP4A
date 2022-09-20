606ff032b7ff46b1af3ed74633d7e202ee7720aa
Céliane <celianeallaire@gmail.com>
2022-09-20 12:29:49 +0200

    change HelloWorld code

--- TP1/HelloWorld.cpp
+++ TP1/HelloWorld.cpp
@@ -3,5 +3,5 @@
 //to not do std: every single time enter "using namespace..."
 
 int main () {
-    std::cout << "Hello World" << std::endl;
+    std::cout << "Hello World, how are you?" << std::endl;
 }
