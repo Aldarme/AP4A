2faa913abd1bc4330e5af5e4251138fcaa7beb51
Swarell <aurelien.petra@gmail.com>
2022-09-20 12:38:47 +0200

    HelloWorld V1

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char c[20];
+    cout << "Entrez un mot" << endl;
+    cin >> c;
+    cout << "Votre mot : " << c << endl;
+    return 0;
+}
