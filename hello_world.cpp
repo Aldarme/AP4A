85bf0e2c152926616db3778946658fe60b21c3cf
timothewt <timothe.wt@gmail.com>
2022-09-25 10:35:41 +0200

    added input in hello world

---
+++ hello_world.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl << "Input two integers:" << endl;
+    int a, b;
+    cin >> a;
+    cin >> b;
+    cout << a << " + " << b << " = " << a + b << endl;
+    cout << a << " - " << b << " = " << a - b << endl;
+    cout << a << " * " << b << " = " << a * b << endl;
+    return 0;
+}
