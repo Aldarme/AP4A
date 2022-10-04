b2198af80e6707f983b486ff1cb627ea4c329590
FEIYUHUI <feiyuhui0822@gmail.com>
2022-10-04 23:14:48 +0200

    23:14 04/10/2022

--- Projet C++/SCHEDULER.h
+++ Projet C++/SCHEDULER.h
@@ -8,10 +8,24 @@
 #include "PRESSION.cpp"
 #include "LIGHT.cpp"
 #include "HUMIDITY.cpp"
+#include "SENSOR.h"
+#include "SERVER.h"
 
 class Scheduler
 {
 private:
+	Humidity m_humidity;
+	Light m_light;
+	Pression m_pression;
+	Temperature m_temperature;
+	int t_durer;
+	void setTimeDurer();
+public:
+	Scheduler();
+	Scheduler(const Scheduler& scheduler_p);
+	~Scheduler();
+	Scheduler& operator=(const Scheduler& scheduler_p);
+	void Simulation();
 
 };
 #endif
