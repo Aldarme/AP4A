8246bae3ecdb3f46db19cd832bcc187c3566ed3f
FEIYUHUI <feiyuhui0822@gmail.com>
2022-10-04 13:51:46 +0200

    04/10/2022

--- Projet1/Projet1.cpp
+++ Projet1/Projet1.cpp
@@ -2,5 +2,56 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
+
+template<class T>;
+class Server {
+    ~Server();
+    Server();
+    
+    Server<T>::Server{
+        void fileWrite() {}
+
+        void consolWrite() {}    
+
+    }
+        Server<T>::~Server{
+            Server & operator=(const Server & autre);
+    }
+};
+
+class Scheduler {
+    Scheduler() {
+        void scheduler();
+    }
+    ~Scheduler() {};
+};
+
+class Sensor {
+    public:Sensor() {
+        
+    }
+
+};
+
+class Temperature :public Sensor{
+    public:
+        float tem;
+};
+
+class Humidity :public Sensor {
+    public:
+        float hum;
+
+};
+
+class Light :public Sensor {
+    public:
+        bool lig;
+};
+
+class Pression :public Sensor {
+    public:
+        int pre;
+};
