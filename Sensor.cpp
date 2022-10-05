aa81c4749758ed5dae999facc304c496177b7e54
Tom <tom.palleau@gmail.com>
2022-10-05 18:13:54 +0200

    [Sensor] +getData returns float value from interval

--- Sensor.cpp
+++ Sensor.cpp
@@ -1,2 +1,12 @@
 #include "Sensor.hpp"
 #include "iostream"
+
+float Sensor::aleaGenVal()
+{
+    return static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+}
+
+float Sensor::getData()
+{
+    return (m_MIN_VAL + aleaGenVal()) * (m_MAX_VAL - m_MIN_VAL);
+}
