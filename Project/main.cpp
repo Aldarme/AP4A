4cd69b10a3e9d89f7099e1f5d49ff9d260134958
Muna Ayuni <munaayuni01@gmail.com>
2022-10-05 12:43:54 +0200

    File main of the project

---
+++ Project/main.cpp
@@ -0,0 +1,15 @@
+#include "Server.hpp"
+#include "Scheduler.hpp"
+#include "Sensor.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+Scheduler sched ;
+Server s ;
+while (true)
+sched.takedata();
+
+}
