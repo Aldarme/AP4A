a148ea9964990d6eb39752ccb7939cd90c1c2c44
Jules Ferlin <jules.ferlin@gmail.com>
2022-10-07 23:23:14 +0200

    Suppression dossier Projet_AP4A

--- Projet_AP4A/cmake-build-debug/CMakeFiles/Projet_AP4A.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Projet_AP4A.
-# This may be replaced when dependencies are built.
--- Projet_AP4A/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "scheduler.hpp"
-
-int main() {
-    Scheduler s;
-    s.startSimulation();
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
