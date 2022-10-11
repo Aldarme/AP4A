2fb62c97b39b65b5a2f1795afb591df41cbf07fd
EmilienRNN <renon.emilien@gmail.com>
2022-10-11 08:56:58 +0200

    Ecriture dans différents fichiers en fonction du capteur

--- PROJET_AP4A/Server.cpp
+++ PROJET_AP4A/Server.cpp
@@ -14,11 +14,50 @@ void Server::consoleWrite(const Sensor<T> &param_sensor1){
 }
 
 template <class T>
-void Server::fileWrite(const Sensor<T> &param_sensor1){
-    ofstream myFile("C:/Users/renon/.vscode/LABAP4A/PROJET_AP4A/data.csv", ios::app); //Déclaration du flux et ouverture du fichier
+void Server::fileWrite(const Temperature<T> &param_sensor){
+    ofstream myFile("C:/Users/renon/.vscode/LABAP4A/PROJET_AP4A/data_temperature.csv", ios::app); //Déclaration du flux et ouverture du fichier
     if(myFile)
     {
-        myFile << param_sensor1<<endl;
+        myFile << param_sensor<<endl;
+        myFile.close();
+    }else
+    {
+        cout<<"Erreur d'ouverture"<<endl;
+    }
+}
+
+template <class T>
+void Server::fileWrite(const Pression<T> &param_sensor){
+    ofstream myFile("C:/Users/renon/.vscode/LABAP4A/PROJET_AP4A/data_pression.csv", ios::app); //Déclaration du flux et ouverture du fichier
+    if(myFile)
+    {
+        myFile << param_sensor<<endl;
+        myFile.close();
+    }else
+    {
+        cout<<"Erreur d'ouverture"<<endl;
+    }
+}
+
+template <class T>
+void Server::fileWrite(const Humidity<T> &param_sensor){
+    ofstream myFile("C:/Users/renon/.vscode/LABAP4A/PROJET_AP4A/data_humidity.csv", ios::app); //Déclaration du flux et ouverture du fichier
+    if(myFile)
+    {
+        myFile << param_sensor<<endl;
+        myFile.close();
+    }else
+    {
+        cout<<"Erreur d'ouverture"<<endl;
+    }
+}
+
+template <class T>
+void Server::fileWrite(const Light<T> &param_sensor){
+    ofstream myFile("C:/Users/renon/.vscode/LABAP4A/PROJET_AP4A/data_light.csv", ios::app); //Déclaration du flux et ouverture du fichier
+    if(myFile)
+    {
+        myFile << param_sensor<<endl;
         myFile.close();
     }else
     {
--- PROJET_AP4A/Server.hpp
+++ PROJET_AP4A/Server.hpp
@@ -12,6 +12,10 @@
 #include <iostream>
 #include <fstream>
 #include "Sensor.hpp"
+#include "Temperature.hpp"
+#include "Pression.hpp"
+#include "Humidity.hpp"
+#include "Light.hpp"
 /**
  * @class Server
  * @brief La classe Server permettra de recevoir les données envoyées par le Scheduler et aussi d'écrire dans la console ou dans un fichier ces données.
@@ -32,17 +36,46 @@ public:
      * @param 4 sensors
      */ 
     template <class T>
-    void consoleWrite(const Sensor<T> &param_sensor1);
+    void consoleWrite(const Sensor<T> &param_sensor);
+
 
     /**
      * @brief Ecrire les données reçues dans un fichier
      * @return void
-     * @param 4 sensors 
+     * @param  Temperature 
      */
     template <class T>
-    void fileWrite(const Sensor<T> &param_sensor1);
+    void fileWrite(const Temperature<T> &param_sensor);
+
+    /**
+     * @brief Ecrire les données reçues dans un fichier
+     * @return void
+     * @param Humidity 
+     */
+    template <class T>
+    void fileWrite(const Humidity<T> &param_sensor);
+
+    /**
+     * @brief Ecrire les données reçues dans un fichier
+     * @return void
+     * @param Pression
+     */
+    template <class T>
+    void fileWrite(const Pression<T> &param_sensor);
+
+    /**
+     * @brief Ecrire les données reçues dans un fichier
+     * @return void
+     * @param Light
+     */
+    template <class T>
+    void fileWrite(const Light<T> &param_sensor);
     
     template <class T> friend class Sensor;
+    template <class T> friend class Temperature;
+    template <class T> friend class Humidity;
+    template <class T> friend class Pression;
+    template <class T> friend class Light;
 };
 
 
