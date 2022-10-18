10fdab4152576bbfa76fc08e84c9815928e05eb5
ArthurBoulet <113430408+ArthurBoulet@users.noreply.github.com>
2022-10-18 22:13:56 +0200

    Add files via upload
    version final classe

--- classe.hpp
+++ classe.hpp
@@ -4,63 +4,112 @@
 #include <fstream>
 #include <string>
 #include <cstdlib>
-class sensor {
-    public:
-    sensor();
-    sensor (const sensor);
-    ~sensor();
-    sensor &operator=(const sensor&);
-    /** générer des valeurs aléatoires aleaGenVal*/
-    int aleaGenVal(){
-        std::defaut_random_engine re;
-        std::uniform_int_distribution<int> distrib{1,6};
-    }
-    /**Demande de données getData*/
-    template <class T> T getData (){
-        
-        T a = aleaGenVal()
-        return a
-    }
-class sensor::humidity{
-    aleagenval .... 
-    float getData()
+#include <ctime>
+#include <random>
+#include <list>
+
+class Sensor {
+    private:
     
-
-}
-
-}
-
-class scheduler {
-    public:
-    scheduler();
-    scheduler (const scheduler);
-    ~scheduler();
-    scheduler &operator=(const scheduler&);
-    /** Notion de delai pour envoyer les données à la classe server */
+    std::string type="sensor";
    
-    server.fileWrite(sensor.getData())
-    server.consoleWrite(sensor.getData())
-}
-
-class server {
     public:
-    server();
-    server (const server);
-    ~server();
-    server &operator=(const server&);
-    void consoleWrite(a){
+    Sensor();
+    Sensor (const Sensor&);
+    ~Sensor();
+    Sensor &operator=(const Sensor&);
+    void changeType (std::string new_type){
+        this->type = new_type;
+    };
+    std::string getType (){
+        return this->type;
+    };
+    template <class T> T aleaGenVal(){
+        T r ;
+        return r;
+
+    };
+    /**Demande de données getData*/
+    template<class T> T getData (){
+        
+        T a = aleaGenVal();
+        return a;
+    };
+
+
+};
+class Scheduler {
+    private:
+    std::list <Sensor> c;
+    Server s;
+    public:
+    Scheduler(Server s,std::list <Sensor> c){
+        this->s = s;
+        this->c = c;
+    };
+    Scheduler (const Scheduler&);
+    ~Scheduler();
+    Scheduler &operator=(const Scheduler&);
+    
+    
+    
+    void ecritureServeur(){
+        for (Sensor elem : c){
+
+            s.fileWrite(elem.getData(),elem);
+            s.consoleWrite(elem.getData());
+        };
+
+    
+    
+    };
+};
+
+class Server {   
+    public:
+    Server();
+    Server (const Server&);
+    ~Server();
+    Server &operator=(const Server&);
+    template <class T> void consoleWrite(T a){
 
        cout << a  ;
-        }
+    };
 
-    template <class T> void fileWrite(T a){
+    template <class T> void fileWrite(T a, Sensor s){
         fstream fichier;
-        fichier.open("capteur.txt", ios::out);
-        fichier >> a;
-        fichier.close();
+        std::string t = s.getType();
+        if (t=="Temperature"){
+            fichier.open("Temperature.txt", ios::out);
+            fichier >> a;
+            fichier.close();
+            sleep(1);
  
-    }
-}
+        };
+
+        elif (t=="Humidity"){
+            fichier.open("Humidity.txt", ios::out);
+            fichier >> a;
+            fichier.close();
+            sleep(3);
+        };
+        elif (t=="Light"){
+            fichier.open("Light.txt", ios::out);
+            fichier >> a;
+            fichier.close();
+            sleep(5);
+ 
+        };
+
+        else {
+            fichier.open("Pression.txt", ios::out);
+            fichier >> a;
+            fichier.close();
+            sleep(10);
+        };
+ 
+    };
+};
 #endif // classe.hpp 
 
 
