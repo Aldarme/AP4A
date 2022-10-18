3fa6cac0ab24145506382481382ff7deabb7150d
NoorElH <99421018+NoorElH@users.noreply.github.com>
2022-10-18 23:28:47 +0200

    Update temperature.h

--- temperature.h
+++ temperature.h
@@ -1,7 +1,12 @@
-#pragma once
+/*
+∗ El Haddad Noor
+∗ Temperature.h
+∗ 18 / 10 / 2022
+∗ Définition de la classe du capteur de temperature
+*/
 #include "sensor.h"
 
-class temperature : public sensor
+class Temperature : public Sensor
 {
-
+	
 };
