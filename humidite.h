3ea3f44d46d9021b8012d232878029c815e17fc2
NoorElH <99421018+NoorElH@users.noreply.github.com>
2022-10-18 23:22:42 +0200

    Update humidite.h

--- humidite.h
+++ humidite.h
@@ -1,7 +1,12 @@
-#pragma once
+/*
+∗ El Haddad Noor
+∗ Humidite.h
+∗ 18 / 10 / 2022
+∗ Classe du capteur d'humidite
+*/
 #include "sensor.h"
 
-class humidite : public sensor
+class Humidite : public Sensor
 {
 
 };
