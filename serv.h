5a624679090e17b711f4ec90cb52df38a10e7bb2
NoorElH <99421018+NoorElH@users.noreply.github.com>
2022-10-18 23:28:15 +0200

    Update serv.h

--- serv.h
+++ serv.h
@@ -1,21 +1,27 @@
+/*
+∗ El Haddad Noor
+∗ serv.h
+∗ 18 / 10 / 2022
+∗ Définition de la Classe du serveur
+*/
 #pragma once
 #include <iostream>
 #include <string>
-using namespace std;
 
-class serveur
+
+class Serveur
 {
 private :
 	float d_temp;
 	float d_hum;
-	float d_li;
-	float d_press;
+	bool d_li;
+	int d_press;
 public:
-	serveur();
-	~serveur();
+	Serveur();
+	~Serveur();
 	void consolWrite();
 	void fileWrite();
-	void SetDonnees(float a,float b,float c,float d);
+	void SetDonnees(float a,float b,bool c,int d);
 
 
 };
