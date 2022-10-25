b3d3e50cec12088f94c9ed1844b22177b66dbff5
zatmax <84315987+zatmax@users.noreply.github.com>
2022-10-25 19:54:21 +0200

    Add files via upload
    Ajout des fichiers version sans modifications finales

---
+++ Rendu_Final_SZATKOWSKI_Maxime/Light.cpp
@@ -0,0 +1,37 @@
+/**
+* @author SZATKOWSKI Maxime
+* @file Ligth.cpp
+* @date 06/10/2022
+* @brief Fichier source contenant les méthodes de la classe Light
+*/
+
+#include "Light.hpp"
+
+//Constructeur par défaut
+Light::Light()
+{
+    this->m_maxValue = 1;   //Présence Lumière 
+    this->m_minValue = 0;   //Absence de Lumière
+}
+
+//Constructeur par recopie
+Light::Light(const Light& temp_p)
+{
+    this->m_maxValue = temp_p.m_maxValue;
+    this->m_minValue = temp_p.m_minValue;
+}
+
+//Destructeur par défaut 
+Light::~Light() 
+{
+
+}
+
+//opérateur d'affectation
+Light& Light::operator=(const Light & temp_p)
+{
+    this->m_minValue = temp_p.m_minValue;
+    this->m_maxValue = temp_p.m_maxValue;
+
+    return *this;
+}
